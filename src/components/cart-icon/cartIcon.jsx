71c2df6df11f14f685b9a69601db0546bc868164
Pallavi Gawai <pallavigawai2022@gmail.com>
2023-10-04 17:23:13 +0530

    cartdrodown button toggled

--- src/components/cart-icon/cartIcon.jsx
+++ src/components/cart-icon/cartIcon.jsx
@@ -8,7 +8,7 @@ const CartIcon = () => {
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
-    <div className="cart-icon-container">
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
